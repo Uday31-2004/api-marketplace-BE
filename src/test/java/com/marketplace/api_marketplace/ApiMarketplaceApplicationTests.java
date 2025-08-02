943a576341c0d6328af19920ddb23c11b6313caa
Uday31-2004 <135055066+Uday31-2004@users.noreply.github.com>
2025-08-03 02:05:41 +0530

    Java app initialized

---
+++ src/test/java/com/marketplace/api_marketplace/ApiMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.api_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
